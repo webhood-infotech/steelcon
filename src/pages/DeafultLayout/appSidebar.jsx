be931acad292c824a2d1f7b8ce4a2ac07b6464ce
and <dwivediakanksha74@gmail.com>
2025-03-25 22:33:03 +0530

    refactor appSidebar component to remove unused items and simplify structure

--- src/pages/DeafultLayout/appSidebar.jsx
+++ src/pages/DeafultLayout/appSidebar.jsx
@@ -37,296 +37,66 @@ const data = {
       url: "/",
       image: "/src/assets/sidebarImages/Icon (1).png",
       isActive: true,
-      items: [
-        {
-          title: "Overview",
-          url: "#",
-        },
-        {
-          title: "Notifications",
-          url: "#",
-        },
-        {
-          title: "Analytics",
-          url: "#",
-        },
-        {
-          title: "Report",
-          url: "#",
-        },
-      ],
     },
     {
       title: "View Policies",
       url: "/view-policies",
       image: "/src/assets/sidebarImages/Icon (8).png",
-      items: [
-        {
-          title: "Genesis",
-          url: "#",
-        },
-        {
-          title: "Explorer",
-          url: "#",
-        },
-        {
-          title: "Quantum",
-          url: "#",
-        },
-      ],
     },
     {
       title: "Manage Department",
       url: "/manage-department",
       image: "/src/assets/sidebarImages/Icon (2).png",
-      items: [
-        {
-          title: "Introduction",
-          url: "#",
-        },
-        {
-          title: "Get Started",
-          url: "#",
-        },
-        {
-          title: "Tutorials",
-          url: "#",
-        },
-        {
-          title: "Changelog",
-          url: "#",
-        },
-      ],
     },
     {
       title: "Manage Managers",
       url: "/manage-managers",
       image: "/src/assets/sidebarImages/layers-three-01 (3).png",
-      items: [
-        {
-          title: "General",
-          url: "#",
-        },
-        {
-          title: "Team",
-          url: "#",
-        },
-        {
-          title: "Billing",
-          url: "#",
-        },
-        {
-          title: "Limits",
-          url: "#",
-        },
-      ],
     },
     {
       title: "Manage Team",
       url: "#",
       image: "/src/assets/sidebarImages/Icon (3).png",
-      items: [
-        {
-          title: "General",
-          url: "#",
-        },
-        {
-          title: "Team",
-          url: "#",
-        },
-        {
-          title: "Billing",
-          url: "#",
-        },
-        {
-          title: "Limits",
-          url: "#",
-        },
-      ],
     },
     {
       title: "Attendance",
       url: "#",
       image: "/src/assets/sidebarImages/Icon (4).png",
-      items: [
-        {
-          title: "General",
-          url: "#",
-        },
-        {
-          title: "Team",
-          url: "#",
-        },
-        {
-          title: "Billing",
-          url: "#",
-        },
-        {
-          title: "Limits",
-          url: "#",
-        },
-      ],
     },
     {
       title: "Leave Application",
       url: "#",
       image: "/src/assets/sidebarImages/Icon (9).png",
-      items: [
-        {
-          title: "General",
-          url: "#",
-        },
-        {
-          title: "Team",
-          url: "#",
-        },
-        {
-          title: "Billing",
-          url: "#",
-        },
-        {
-          title: "Limits",
-          url: "#",
-        },
-      ],
     },
     {
       title: "Payout Details",
       url: "#",
       image: "/src/assets/sidebarImages/Icon (5).png",
-      items: [
-        {
-          title: "General",
-          url: "#",
-        },
-        {
-          title: "Team",
-          url: "#",
-        },
-        {
-          title: "Billing",
-          url: "#",
-        },
-        {
-          title: "Limits",
-          url: "#",
-        },
-      ],
     },
     {
       title: "Discipline Action",
       url: "#",
       image: "/src/assets/sidebarImages/Icon (10).png",
       icon: Settings2,
-      items: [
-        {
-          title: "General",
-          url: "#",
-        },
-        {
-          title: "Team",
-          url: "#",
-        },
-        {
-          title: "Billing",
-          url: "#",
-        },
-        {
-          title: "Limits",
-          url: "#",
-        },
-      ],
     },
     {
       title: "Offer Letter",
       image: "/src/assets/sidebarImages/Icon (8).png",
-      items: [
-        {
-          title: "General",
-          url: "#",
-        },
-        {
-          title: "Team",
-          url: "#",
-        },
-        {
-          title: "Billing",
-          url: "#",
-        },
-        {
-          title: "Limits",
-          url: "#",
-        },
-      ],
     },
     {
       title: "Announcement",
       image: "/src/assets/sidebarImages/Icon (11).png",
       icon: Settings2,
-      items: [
-        {
-          title: "General",
-          url: "#",
-        },
-        {
-          title: "Team",
-          url: "#",
-        },
-        {
-          title: "Billing",
-          url: "#",
-        },
-        {
-          title: "Limits",
-          url: "#",
-        },
-      ],
     },
     {
       title: "Training Module",
       image: "/src/assets/sidebarImages/Icon (12).png",
       icon: Settings2,
-      items: [
-        {
-          title: "General",
-          url: "#",
-        },
-        {
-          title: "Team",
-          url: "#",
-        },
-        {
-          title: "Billing",
-          url: "#",
-        },
-        {
-          title: "Limits",
-          url: "#",
-        },
-      ],
     },
     {
       title: "Company Policies",
       image: "/src/assets/sidebarImages/Icon (7).png",
       icon: Settings2,
-      items: [
-        {
-          title: "General",
-          url: "#",
-        },
-        {
-          title: "Team",
-          url: "#",
-        },
-        {
-          title: "Billing",
-          url: "#",
-        },
-        {
-          title: "Limits",
-          url: "#",
-        },
-      ],
     },
   ],
   navSupportSetting: [
@@ -335,48 +105,14 @@ const data = {
       url: "#",
       image: "/src/assets/sidebarImages/life-buoy-01.png",
       isActive: true,
-      items: [
-        {
-          title: "Overview",
-          url: "#",
-        },
-        {
-          title: "Notifications",
-          url: "#",
-        },
-        {
-          title: "Analytics",
-          url: "#",
-        },
-        {
-          title: "Report",
-          url: "#",
-        },
-      ],
+      
     },
     {
       title: "Settings",
       url: "#",
       image: "/src/assets/sidebarImages/settings-01 (1).png",
       isActive: true,
-      items: [
-        {
-          title: "Overview",
-          url: "#",
-        },
-        {
-          title: "Notifications",
-          url: "#",
-        },
-        {
-          title: "Analytics",
-          url: "#",
-        },
-        {
-          title: "Report",
-          url: "#",
-        },
-      ],
+     
     },
   ],
 };
